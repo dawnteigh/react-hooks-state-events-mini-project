94ea399674287de9589c64ef459c5f3647d71a26
Donte Ladatto <donte279@gmail.com>
2022-04-12 23:07:30 -0500

    doneskies

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,12 +8,49 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+
+  const [taskList, setTaskList] = useState(TASKS)
+  const [categoryList, setCategoryList] = useState("All")
+  const [filter, setFilter] = useState(taskList)
+
+  function handleFilter(e) {
+    setCategoryList(e.target.value)
+    if (e.target.value === "All") {
+      setFilter(taskList)
+    } else {
+      let categoryList = taskList.filter(task => task.category === e.target.value)
+      setFilter(categoryList)
+    }
+  }
+
+  function handleDelete(deletedTask) {
+    let tempList = taskList.filter(task => task.text !== deletedTask)
+    setTaskList(tempList)
+    if (categoryList === "All") {
+      setFilter(tempList)
+    } else {
+      let filteredTasks = tempList.filter(task => task.category === categoryList)
+      setFilter(filteredTasks)
+    }
+  }
+
+  function onTaskFormSubmit(newTask) {
+    setTaskList([...taskList, newTask])
+    if (categoryList === "All") {
+      setFilter([...taskList, newTask])
+    } else {
+      let filteredTasks = [...taskList, newTask].filter(task => task.category === categoryList)
+      setFilter(filteredTasks)
+    }
+
+  }
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter handleFilter={handleFilter} categoryList={categoryList} categories={CATEGORIES} />
+      <NewTaskForm onTaskFormSubmit={onTaskFormSubmit} categories={CATEGORIES} />
+      <TaskList handleDelete={handleDelete} tasks={filter} />
     </div>
   );
 }
