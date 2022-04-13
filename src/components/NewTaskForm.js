1906a9b6e31bc0b1bf491162cfabd9a54ba081b5
Donte Ladatto <donte279@gmail.com>
2022-04-12 23:06:58 -0500

    commit tree 00b84d9dda5ea49733b47379138d84fad826bc36

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm({ categories, onTaskFormSubmit }) {
+  let [textState, setTextState] = useState("")
+  let [catState, setCatState] = useState("Code")
+
+  function handleTextState(e) {
+    setTextState(e.target.value)
+  }
+
+  function handleCatState(e) {
+    setCatState(e.target.value)
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    onTaskFormSubmit({
+      text: textState,
+      category: catState
+    })
+  }
+
+  let catDropdown = categories.filter(category => category !== "All")
+
   return (
-    <form className="new-task-form">
+    <form onSubmit={handleSubmit} className="new-task-form">
       <label>
         Details
-        <input type="text" name="text" />
+        <input onChange={handleTextState} type="text" name="text" />
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select onChange={handleCatState} name="category">
+          {catDropdown.map(category =>
+          (<option
+            value={category}
+            key={category}>
+            {category}
+          </option>))}
         </select>
       </label>
       <input type="submit" value="Add task" />
@@ -19,3 +45,4 @@ function NewTaskForm() {
 }
 
 export default NewTaskForm;
+
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,16 @@
 import React from "react";
 
-function Task() {
+function Task({ text, category, handleDelete }) {
+
+  function messySolution() {
+    handleDelete(text)
+  }
+
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{category}</div>
+      <div className="text">{text}</div>
+      <button onClick={messySolution} className="delete">X</button>
     </div>
   );
 }
